e8d8a4e3638dd939f5665c6330c7c12b7b3f8403
Joshua <joshuagpatten@gmail.com>
2022-03-12 14:46:50 -0500

    make calculator UI using HTML & CSS

---
+++ myscripts.js
@@ -0,0 +1,21 @@
+function operate(operator, op1, op2){
+    switch (operator) {
+    case '+':
+        return (op1 + op2);
+        break;
+    case '-':
+        return (op1 - op2);
+        break;
+    case '*':
+        return (op1 * op2);
+        break;
+    case '/':
+        return (op1 / op2);
+        break;
+    
+    default:
+        break;
+}
+
+}
+
