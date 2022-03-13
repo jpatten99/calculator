5da282a39e188f0545e650dec86dd77ac01d1861
Joshua <joshuagpatten@gmail.com>
2022-03-12 21:57:57 -0500

    make her a little prettier with a switch case, add clear button

--- myscripts.js
+++ myscripts.js
@@ -3,6 +3,8 @@ let operand2 = '';
 let operatorClicked = false;
 let operator = '';
 let runningTotal = 0;
+let equalClicked = false;
+
 const display = document.getElementById('display');
 if(operand1===''){
     document.getElementById("+").disabled = true;
@@ -35,19 +37,77 @@ function operate(operator, op1, op2){
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', () =>{
+        switch(button.id){
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                if(!operatorClicked){
+                    operand1+=button.id;
+                    if(operand1 !== '0') operand1 = operand1.replace(/^0+/, '');
+                    console.log(operand1);
+                }
+                else{
+                    operand2 += button.id;
+                    if(operand2 !== '0') operand2 = operand2.replace(/^0+/, '');
+                    console.log(operand2);
+                }
+                if(operand1){
+                    document.getElementById("+").disabled = false;
+                    document.getElementById("-").disabled = false;
+                    document.getElementById("*").disabled = false;
+                    document.getElementById("/").disabled = false;
+                }
+                if(operand2){document.getElementById("=").disabled = false;}
+                break;
+            case '+':
+            case '-':    
+            case '*':
+            case '/':
+                operatorClicked = true;
+                operator = button.id;
+                break;
+            case '=':
+                equalClicked = true;
+                runningTotal = operate(operator, operand1, operand2);
+                display.textContent = operate(operator, operand1, operand2);       
+                break;    
+            case 'CLR':
+                operand1 = '';
+                operand2 = '';
+                operatorClicked = false;
+                operator = '';
+                runningTotal = 0;
+                equalClicked = false;
+                document.getElementById("+").disabled = true;
+                document.getElementById("-").disabled = true;
+                document.getElementById("*").disabled = true;
+                document.getElementById("/").disabled = true;
+                document.getElementById("=").disabled = true;
+                
+        }
+        if(!equalClicked){display.textContent = operand1 + operator + operand2;}
+
+       /*
         //checks if any operator is clicked, if so set flag and assign it to operator variable
         if(button.id === '+' || button.id === '-' || button.id === '*' || button.id === '/'){
             operatorClicked = true;
             operator = button.id;
         }
         //if operator isn't clicked and not a bad button.id, we know we're still adding to op1
-        if(!operatorClicked && button.id !== '+' && button.id !== '-' && button.id !== '*' && button.id !== '/'){
+        if(!operatorClicked && button.id !== '+' && button.id !== '-' && button.id !== '*' && button.id !== '/' && button.id !== 'CLR'){
             operand1+=button.id;
             if(operand1 !== '0') operand1 = operand1.replace(/^0+/, '');
             
         }
         //if operator is clicked, don't add it to op2, but start adding number values to op2
-        else if(operatorClicked && button.id !== '+' && button.id !== '-' && button.id !== '*' && button.id !== '/' && button.id !== '='){
+        else if(operatorClicked && button.id !== '+' && button.id !== '-' && button.id !== '*' && button.id !== '/' && button.id !== '=' && button.id !== 'CLR'){
             operand2 += button.id;
             if(operand2 !== '0') operand2 = operand2.replace(/^0+/, '');
             
@@ -64,6 +124,8 @@ buttons.forEach((button) => {
         }
         else{display.textContent=operand1 + operator + operand2;}
 
+        if(button.id)
+
         //following if statement enables +, -, *, / if there's a first operand entered
         if(operand1!==''){
             document.getElementById("+").disabled = false;
@@ -73,7 +135,9 @@ buttons.forEach((button) => {
         }
 
        
-        
+        */
+
+
     });
     
 });
